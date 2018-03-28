1c30bf405af1a7532d68315e9d01135a61b48156
Max <max.laihonen@gmail.com>
2018-03-28 22:10:06 +0300

    Fixfix

--- src/main/java/wine/WebSecurityConfig.java
+++ src/main/java/wine/WebSecurityConfig.java
@@ -25,6 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	        .and()
 	        .authorizeRequests().antMatchers("/winelist").permitAll()
 	        .and()
+	        .authorizeRequests().antMatchers("/index").permitAll()
+	        .and()
 	        .authorizeRequests().anyRequest().authenticated()
 	        .and()
 	      .formLogin()
